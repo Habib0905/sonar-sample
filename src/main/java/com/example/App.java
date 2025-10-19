c74c30eaba850cd7787b72281d4795df7293a4f0
Habib Hussain <hussainhabib05@yahoo.com>
2025-10-19 22:37:32 +0600

    added function

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,20 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+        System.out.println("Sum of 3 and 5 is: " + addNumbers(3, 5));
+    }
+
+    
+    public static int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static String greet(String name) {
+        String message = "Hello, User!";
+        return message; // 'name' is ignored → SonarQube will flag this
     }
 }
